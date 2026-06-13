261808db420dcabbfbb50a4ca45764494dc8f1ac
swakraft <hugo.beets2003@gmail.com>
2026-06-14 00:40:20 +0200

    added cors

--- src/server/cors.py
+++ src/server/cors.py
@@ -4,22 +4,12 @@ from init import app
 cors = aiohttp_cors.setup(
     app,
     defaults={
-        "https://contenders.chauffagistes-pool.fr": aiohttp_cors.ResourceOptions(
+        "https://contenders.chauffagistes-btc.fr": aiohttp_cors.ResourceOptions(
             allow_credentials=True,
             expose_headers="*",
             allow_headers="*"
         ),
-        "https://contenders.swakraft.fr": aiohttp_cors.ResourceOptions(
-            allow_credentials=True,
-            expose_headers="*",
-            allow_headers="*"
-        ),
-        "http://localhost:3003": aiohttp_cors.ResourceOptions(
-            allow_credentials=True,
-            expose_headers="*",
-            allow_headers="*"
-        ),
-        "https://hash-contenders.chauffagistes-btc.fr": aiohttp_cors.ResourceOptions(
+        "https://contenders.staging.chauffagistes-btc.fr": aiohttp_cors.ResourceOptions(
             allow_credentials=True,
             expose_headers="*",
             allow_headers="*"
